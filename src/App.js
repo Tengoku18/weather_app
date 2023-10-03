bc18d9400a92da12a6896ae2998915010e4ea011
Tengoku18 <tengokusenpai18@gmail.com>
2023-10-03 23:40:08 +0545

    Project setup with tailwind CSS

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="text-3xl font-semibold text-blue-600">
+        {" "}
+        Hello Dear, wanna get some freeplay.{" "}
+      </div>
+    </div>
+  );
+}
+
+export default App;
